fcde88092c2bdd2867cbfdbba93caa78b4e65975
AlPal <arudelic@umich.edu>
2021-11-26 19:39:38 -0500

    Fixed broken vue, finished up locate page

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+Vue.config.devtools = true;
+
 var resultView = new Vue({
   el: '#app',
   data: {
     curPage: 'main',
     locateOptions: new Set(),
+    locateDistance: 0,
+    locateResults: [],
   },
   methods: {
     addLocateOption(option) {
@@ -22,16 +26,19 @@ var resultView = new Vue({
     handleLocate() {
       axios
           .get('https://sheets.googleapis.com/v4/spreadsheets/1w8qms9wIXwbTyU_N0tBiNIii3t3-t_OIwmKTX6RSc08/values/A2:J1000?key=AIzaSyAyPyz8Df6KljEVecXIsxRRNhEe1QmRTMA')
-          .then(response => (handleSheetsResponse(response)));
+          .then(response => (this.handleSheetsResponse(response)));
     },
 
     //TODO | @RAUL: This function returns true or false depending on whether the given location is within this.locateDistance from the user
     //              Will return False when the given location is too far away.
     withinUserRange(lat, long) {
-      return false;
+      return true;
     },
 
     handleSheetsResponse(response) {
+      //Clear results array
+      this.locateResults = [];
+
       for (i = 0; i < Object.keys(response.data.values).length; ++i) {
         let valid = true;
         let options = ['landfill', 'recycle', 'bottle', 'can', 'chemical', 'electronic']
@@ -47,16 +54,16 @@ var resultView = new Vue({
         }
 
         //Check if distance is within preferred user range
-        if (!withinUserRange(response.data.values[i][1], response.data.values[i][2])) {
+        if (!this.withinUserRange(response.data.values[i][1], response.data.values[i][2])) {
           valid = false;
         }
 
         //Populate locateResults with valid locations
         if (valid) {
-          this.locateResults.append(response.data.values[i]);
+          this.locateResults.push(response.data.values[i]);
         }
       }
-      console.log(this.locateResults)
+      console.log(this.locateResults);
     }
 
   }
